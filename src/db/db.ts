5277306705c2e19b2ea30febde837dd2f06dba46
Mark Doyle <mark@markdoyle.dev>
2020-10-26 18:09:58 +0000

    feat: update mongo connection uri

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,11 +1,12 @@
 import monk from "monk";
+import * as dotenv from "dotenv";
 
 import Airport from "../types/airport";
 
-const URL = "localhost:27017/airports";
+dotenv.config();
 
-export const db = monk(URL);
-export const airports = db.get("all");
+export const db = monk(process.env.MONGO_URL || "");
+export const airports = db.get("airports");
 
 export const getAirport = async (_: null, {
   icao,
