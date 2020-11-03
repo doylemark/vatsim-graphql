daf73a14594e57b5401f812e255c448c0680ae3a
Mark Doyle <mark@markdoyle.dev>
2020-11-03 21:02:56 +0000

    feat: rollback & fix misimplemented filtering

--- src/db/db.ts
+++ src/db/db.ts
@@ -8,22 +8,21 @@ dotenv.config();
 export const db = monk(process.env.MONGO_URL || "");
 export const airports = db.get("airports");
 
-export const getAirport = async (icao: string): Promise<Airport> => {
+export const getAirport = async (icao: string): Promise<Airport | undefined> => {
   const airport: Airport | undefined = await airports.findOne({ icao });
 
   if (!airport) {
-    return {
-      icao: null,
-      iata: null,
-      name: null,
-      city: null,
-      state: null,
-      country: null,
-      elevation: null,
-      lat: null,
-      lon: null,
-      tz: null,
-    };
+    return undefined;
+  }
+
+  return airport;
+};
+
+export const getAirportByIata = async (iata: string): Promise<Airport | undefined> => {
+  const airport: Airport | undefined = await airports.findOne({ iata });
+
+  if (!airport) {
+    return undefined;
   }
 
   return airport;
